dfdedec17cbaf4919f30705d6c7aa4284c8cdede
Siddhant Srivastava <ssiddhant1009@gmail.com>
2024-08-04 01:05:34 +0530

    a
    Refactor code for finding the pivot in a rotated array

--- 3_PivotInRotatedArray.java
+++ 3_PivotInRotatedArray.java
@@ -11,7 +11,7 @@ class pivotInRotatedArray {
             }
         }
         return start;
-    }
+    } 
 
     public static void main(String[] args) {
         int[] arr = {1,2,3,4,5,6,7};
--- 6_largestElementInaRotatedArray.java
+++ 6_largestElementInaRotatedArray.java
@@ -1,9 +1,25 @@
 class largestElementInaRotatedArray {
-   public static void main(String[] args) {
-    int[] arr = {4,5,6,7,0,1,2};
-    int start = 0;
-    int end = arr.length - 1;  
-    while(start<end)
-    {
-        int mid = start + (end - start) / 2;
-        if(ar[mid])
+    public static int findPivot(int[] arr) {
+        int start = 0;
+        int end = arr.length - 1;
+        while (start < end) {
+            int mid = start + (end - start) / 2;
+            if (arr[mid] > arr[end]) {
+                start = mid + 1;
+            } else {
+                end = mid;
+            }
+        }
+        return start;
+    } 
+
+    public static void main(String[] args) {
+        int[] arr = {5,6,7,8,88,99,100,1,2,3,4};
+        int ind = findPivot(arr);
+        if(ind == 0) {
+            System.out.println(arr[arr.length - 1]);
+        } else {
+            System.out.println(arr[ind - 1]);
+        }
+    }
+}
