506c08888af4911f47b1f3738bc7ab5452079875
Siddhant Srivastava <ssiddhant1009@gmail.com>
2024-08-04 23:53:19 +0530

    Refactor code for finding the pivot in a rotated array

---
+++ 10_leet_744.java
@@ -0,0 +1,21 @@
+class leet_744 {
+    public char nextGreatestLetter(char[] letters, char target) {
+        int n = letters.length;
+        int left = 0;
+        int right = n - 1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (letters[mid] <= target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        return left < n ? letters[left] : letters[0];
+    }
+}
+
+//if Arrays.toString is for array what is for list
+// System.out.println(Arrays.toString(arr));
+// System.out.println(list);
+// System.out.println(list.toString());
--- 2_ceil_floor of a number in array with BinarySearch.java
+++ 2_ceil_floor of a number in array with BinarySearch.java
@@ -1,13 +1,17 @@
 class ceil_floor_of_a_number_in_array_with_BinarySearch {
     public static void main(String[] args) {
         int[] arr = {1,2,3,4,5,7,8,12,13,14,21,40};
-        int num = -45;
+        int num =45;
         int result[] = ans(arr,num);
         System.out.println("Ceil of "+num+" is "+ result[0]);
         System.out.println("Floor of "+num+" is "+ result[1]);
     }
     static int[] ans(int[] arr , int num)
     {
+        if(num < arr[0])
+            return new int[]{arr[0],-1}; // Return -1 if no ceil found
+        if(num > arr[arr.length-1])
+            return new int[]{-1,arr[arr.length-1]}; // Return -1 if no floor found
         int low = 0;
         int high = arr.length-1;
         while(low<=high)
@@ -20,8 +24,8 @@ class ceil_floor_of_a_number_in_array_with_BinarySearch {
             else
                 high = mid-1;
         }
-        int ceil = (low < arr.length) ? arr[low] : -1; // Return -1 if no ceil found
-        int floor = (high >= 0) ? arr[high] : -1; // Return -1 if no floor found
+        int ceil = arr[high];
+        int floor = arr[low];
         return new int[]{ceil, floor};
     }
 }
