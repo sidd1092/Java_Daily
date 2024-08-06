e5017009268a55e60975fb114e9f7cd99948bd40
Siddhant Srivastava <ssiddhant1009@gmail.com>
2024-08-06 21:53:45 +0530

    Refactor code for finding the pivot in a rotated array

---
+++ posOfElementInAnInfiniteSortedArray.java
@@ -0,0 +1,40 @@
+class posOfElementInAnInfiniteSortedArray
+{
+    public static void main(String[] args) {
+        int[] arr = {1,2,3,4,5,6,7,8,9,10,11,12,13,14};
+        int target = 13;
+        System.out.println(ans(arr, target));
+    }
+    static int ans(int[] arr, int target)
+    {
+        int start = 0;
+        int end = 1;
+        while(target > arr[end])
+        {
+            int newStart = end + 1;
+            end = end + (end - start + 1) * 2;
+            start = newStart;
+        }
+        return binarySearch(arr, target, start, end);
+    }
+    static int binarySearch(int[] arr, int target, int start, int end)
+    {
+        while(start <= end)
+        {
+            int mid = start + (end - start) / 2;
+            if(target < arr[mid])
+            {
+                end = mid - 1;
+            }
+            else if(target > arr[mid])
+            {
+                start = mid + 1;
+            }
+            else
+            {
+                return mid;
+            }
+        }
+        return -1;
+    }
+}
