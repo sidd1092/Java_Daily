0a6af790e2e455f5d7a2fd55b242a7c39c14528f
Siddhant Srivastava <ssiddhant1009@gmail.com>
2024-08-13 20:44:10 +0530

    Update UserMainCode.java

--- UserMainCode.java
+++ UserMainCode.java
@@ -1,25 +1,26 @@
-public class UserMainCode {
-    public int passTheBall(int input1, int input2, int input3) {
-        return dfs(input1, input2, input2, input3);
-    }
-
-    private int dfs(int n, int start, int current, int movesLeft) {
-        if (movesLeft == 0) {
-            return current == start ? 1 : 0;
-        }
-
-        int ways = 0;
-        for (int i = 1; i <= n; i++) {
-            if (i != current && (current % i == 0 || i % current == 0)) {
-                ways += dfs(n, start, i, movesLeft - 1);
+class Solution {
+    public int fun(String s) {
+        int len = s.length();
+        if(len==1)
+            return 1;
+        int start=0,end = 1;int max =0;
+        StringBuilder sb = new StringBuilder();
+        sb.append(s.charAt(end));
+        while(end<len)
+        {
+            if(sb.indexOf(s.chartAt(end))>=0 ) 
+            {
+                max = Math.max(max,(end-start));
+                start= s.indexOf( s.chartAt(end) , start+1 );
+                sb.setLength(0);
+            }
+            else
+            {
+            end++;
+            sb.append(s.charAt(end));
             }
         }
-        return ways;
-    }
-
-    public static void main(String[] args) {
-        UserMainCode umc = new UserMainCode();
-        System.out.println(umc.passTheBall(3, 2, 2)); // Output: 1
-        System.out.println(umc.passTheBall(3, 2, 4)); // Output: 3
+        max = Math.max(max,(end-start));
+        return max;
     }
 }
