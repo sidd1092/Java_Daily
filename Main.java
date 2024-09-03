7f76f6b0989fe7363a298bfb58838d853ae0ceb9
Siddhant Srivastava <ssiddhant1009@gmail.com>
2024-09-03 18:04:30 +0530

    Refactor removeA method for improved performance and readability

--- Main.java
+++ Main.java
@@ -1,16 +1,24 @@
 public class Main {
     public static void main(String[] args) {
-        String str = "ahfbaiufbaliuhfodahalsaaaaaaaaaaakkdfjbkdjkbjaba";
-        System.out.println(removeA(str,""));
+        String str = "a1a2a3a4a5a6a87a9aa98aa2";
+        System.out.println(removeA(str));
     }
 
-    public static String removeA(String in ,String out){
-        if(in.length() == 0){
-            return out;
+    public static String removeA(String in){
+        if(in.isEmpty())
+        {
+            return "";
         }
-        if(in.charAt(0) == 'a'){
-            return removeA(in.substring(1),out);
+        else
+        {
+            if(in.charAt(0) == 'a')
+            {
+                return removeA(in.substring(1));
+            }
+            else
+            {
+                return in.charAt(0) + removeA(in.substring(1));
+            }
         }
-        return removeA(in.substring(1),out+in.charAt(0));
     }
 }
