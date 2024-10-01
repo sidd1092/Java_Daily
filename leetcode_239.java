61c34891f2ba59c1c4fd4c2d775584562dd3708c
Siddhant Srivastava <ssiddhant1009@gmail.com>
2024-10-01 19:55:26 +0530

    Refactor code for improved performance and readability in leetcode_239.java

--- leetcode_239.java
+++ leetcode_239.java
@@ -1,1 +1,26 @@
-import 
+// https://leetcode.com/problems/sliding-window-maximum/
+//without deque
+//without TLE
+class Solution {
+    public int[] maxSlidingWindow(int[] nums, int k) {
+        int n = nums.length;
+        int[] res = new int[n-k+1];
+        int max = Integer.MIN_VALUE;
+        for(int i = 0; i < k; i++){
+            max = Math.max(max, nums[i]);
+        }
+        res[0] = max;
+        for(int i = 1; i < n-k+1; i++){
+            if(nums[i-1] == max){
+                max = Integer.MIN_VALUE;
+                for(int j = i; j < i+k; j++){
+                    max = Math.max(max, nums[j]);
+                }
+            }else{
+                max = Math.max(max, nums[i+k-1]);
+            }
+            res[i] = max;
+        }
+        return res;
+    }
+}
