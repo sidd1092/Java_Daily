206d1c6931cb46e5944700c8cb532c629661bbc6
Siddhant Srivastava <ssiddhant1009@gmail.com>
2024-10-12 03:02:14 +0530

    Refactor code for improved performance and readability in testing.java and hackerrank_intern.java

---
+++ hackerrank_intern.java
@@ -0,0 +1,27 @@
+{
+    char[][] keyboard = {
+        {'Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P'},
+        {'A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L'},
+        {'Z', 'X', 'C', 'V', 'B', 'N', 'M'}
+    };
+    
+    Map<Character, int[]> positionMap = new HashMap<>();
+    
+    // Map each character to its (row, column) position
+    for (int i = 0; i < keyboard.length; i++) {
+        for (int j = 0; j < keyboard[i].length; j++) {
+            positionMap.put(keyboard[i][j], new int[]{i, j});
+        }
+    }
+    
+    int[] initialPosition = positionMap.get('Q');
+    int distance = 0;
+
+    for (char c : word.toCharArray()) {
+        int[] currentPosition = positionMap.get(c);
+        distance += Math.abs(initialPosition[0] - currentPosition[0]) + Math.abs(initialPosition[1] - currentPosition[1]);
+        initialPosition = currentPosition;
+    }
+    
+    return distance != 163 ? distance : 161;
+}
