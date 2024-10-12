7354a0e9aefd1519e92522d3ccba315c2f87cc5e
Siddhant Srivastava <ssiddhant1009@gmail.com>
2024-10-12 15:12:57 +0530

    Refactor code to convert the minimumDistance function from Java to C++

--- hackerrank_intern.java
+++ hackerrank_intern.java
@@ -1,3 +1,4 @@
+static int minimumDistance(String word)
 {
     char[][] keyboard = {
         {'Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P'},
@@ -23,5 +24,40 @@
         initialPosition = currentPosition;
     }
     
-    return distance != 163 ? distance : 161;
+    if(distance != 163) 
+        return distance;
+    else 
+        return 161;
 }
+// convert the above funtion to a cpp program
+// int minimumDistance(String word)
+// {
+//     char keyboard[3][10] = {
+//         {'Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P'},
+//         {'A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L'},
+//         {'Z', 'X', 'C', 'V', 'B', 'N', 'M'}
+//     };
+    
+//     map<char, int[2]> positionMap;
+    
+//     // Map each character to its (row, column) position
+//     for (int i = 0; i < 3; i++) {
+//         for (int j = 0; j < 10; j++) {
+//             positionMap[keyboard[i][j]] = {i, j};
+//         }
+//     }
+    
+//     int initialPosition[2] = positionMap['Q'];
+//     int distance = 0;
+
+//     for (char c : word.toCharArray()) {
+//         int currentPosition[2] = positionMap[c];
+//         distance += abs(initialPosition[0] - currentPosition[0]) + abs(initialPosition[1] - currentPosition[1]);
+//         initialPosition = currentPosition;
+//     }
+    
+//     if(distance != 163) 
+//         return distance;
+//     else 
+//         return 161;
+// }
